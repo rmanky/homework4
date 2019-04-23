a5e964bfa344942ae56dc9ae7bc27bdea108ce86
rmanky <rmankaryous12@gmail.com>
2019-04-23 23:01:25 +0000

    blah

--- question3.py
+++ question3.py
@@ -1,1 +1,12 @@
+"""
+Question 3
+"""
 
+FILEPATH = 'graph.txt'
+with open(FILEPATH) as fp:
+    LINE = fp.readline()
+    CNT = 1
+    while LINE:
+        print("Line {}: {}".format(CNT, LINE.strip()))
+        LINE = fp.readline()
+        CNT += 1
